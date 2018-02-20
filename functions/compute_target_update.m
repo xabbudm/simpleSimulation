710c35a689cd67489d10ab2136959b10141158d0
dmuhle <dmuhle@gmail.com>
2018-02-20 14:42:34 +0100

    start of speed optimizations

--- functions/compute_target_update.m
+++ functions/compute_target_update.m
@@ -1,4 +1,4 @@
-function [target_update] = compute_target_update()
+function [target_update, targets_eaten] = compute_target_update()
 
 global targets indices_foragers;
 
@@ -6,10 +6,17 @@ target_occupation = zeros(size(targets));
 target_occupation(targets > 0) = 1;
 
 worm_occupation = zeros(size(targets));
-worm_occupation(sub2ind(size(targets), indices_foragers(:, 1), indices_foragers(:, 2))) = 1;
+idx = sub2ind(size(targets), indices_foragers(:, 1), indices_foragers(:, 2));
+worm_occupation(idx) = 1;
 
 target_update = zeros(size(targets));
 
-target_update((target_occupation.*worm_occupation) > 0) = -1;
+target_update((target_occupation.*worm_occupation) > 0) = 1;
+
+targets_eaten = zeros(size(indices_foragers, 1), 1);
+targets_eaten(:) = target_update(idx);
+
+target_update = target_update* (-1);
+
 
 end
--- functions/targetDistributionAllSimulations.m
+++ functions/targetDistributionAllSimulations.m
@@ -10,34 +10,45 @@ for ii = 1:allSimulations
     %matrix with indices of targets
     index = zeros(initialNumberTargets * L^2,2);
     index(1,:) = randi(L,1,2);
-    for n = 2:size(index,1)
+    
+    if gamma == 0
+      index = randi(L, initialNumberTargets * L^2, 2);
+    else
+      r_ = rand(size(index, 1), 1);
+      r_in = logical(r_ < (1/(L/sqrt(2)))^gamma);
+      
+      d = zeros(size(r_));
+      d(r_in) = 1 + r_.*((L/sqrt(2))-1);
+      d(~r_in) = r_.^(1-gamma);
+      
+      direction = rand(size(r_))*2*pi;
+      update = zeros(size(r_, 1), 2);
+      update(:, 1) = round(-(d.*sin(direction)));
+      update(:, 2) = round((d.*cos(direction)));
+      for n = 2:size(index,1)
 
-        %compute distance where new target should appear
-        r = rand(1);
-        if gamma == 0
-            index(n,:) = randi(L,1,2);
-        else
-            if r < (1/(L/sqrt(2)))^gamma
-                d = 1 + ((L/sqrt(2))-1) * rand(1);
-            else 
-                d = r^(-1/gamma);
-            end
+          %compute distance where new target should appear
+%          r = rand(1);
+%          if r < (1/(L/sqrt(2)))^gamma
+%              d = 1 + ((L/sqrt(2))-1) * rand(1); % nochmals rand könnte d über Schwelle heben
+%          else 
+%              d = r^(-1/gamma);
+%          end
 
-            %choose existing target as basis
-            basis = randi(n-1);
+          %choose existing target as basis
+          basis = randi(n-1);
 
-            %compute direction of update
-            direction = 2*pi*rand(1);
-            update(1,1) = round(-(d * sin(direction)));
-            update(1,2) = round(d * cos(direction));
-            new_position = index(basis,:) + update(1,:);
-            new_position(new_position > L) = new_position(new_position > L) - L;
-            new_position(new_position < 1) = new_position(new_position < 1) + L;
-            index(n,:) = new_position;
-        end
-        %update target matrix
-        target(index(n,1),index(n,2)) = target(index(n,1),index(n,2)) + 1;
-    end
+          %compute direction of update
+          direction = 2*pi*rand(1);
+          update(1,1) = round(-(d * sin(direction)));
+          update(1,2) = round(d * cos(direction));
+          new_position = index(basis,:) + update(1,:);
+          new_position(new_position > L) = new_position(new_position > L) - L;
+          new_position(new_position < 1) = new_position(new_position < 1) + L;
+          index(n,:) = new_position;
+          %update target matrix
+          target(index(n,1),index(n,2)) = target(index(n,1),index(n,2)) + 1;
+      end
     target(index(1,1),index(1,2)) = target(index(1,1),index(1,2)) + 1;
     
     %save target distribution in 3D matrix
